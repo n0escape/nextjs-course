0c587b2d0898ff702223e87b920be34ca23796ed
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2025-07-28 13:28:55 +0300

    adding styles & not-found-page

---
+++ src/app/[...not-found]/page.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+
+type NotFoundPageProps = {}
+const NotFoundPage: FC<NotFoundPageProps> = ({}) => {
+    return (
+        <>
+            404 not found
+        </>
+    )
+};
+
+export default NotFoundPage;
