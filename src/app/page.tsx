4438c55fafe8deb4162554105fa0616253fd0ffe
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2025-07-28 14:56:16 +0300

    added simple pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,45 @@
-import { getAllArticles } from "./(server)/api";
-import { ROUTING } from "./routing";
-import AppLink from "./shared/compontsts/app-link";
+import { getAllArticles } from './(server)/api';
+import { ROUTING } from './routing';
+import AppLink from './shared/compontsts/app-link';
+const ARTICLES_PER_PAGE = 10;
 
 export const metadata = {
-	title: "NoEscape blog",
-}
+	title: 'NoEscape blog',
+};
 
-export default async function Home() {
-
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Record<string, string>;
+}) {
 	const allArticles = await getAllArticles();
+	const page = Number.parseInt(searchParams['page'] ?? 1);
 
-	return (<>
-		<h1>Education blog</h1>
-		<ul>
-			{allArticles.map((article, index) => (
-				<li key={index}>
-					<AppLink
-						href={ROUTING.article(article.name)}	
-					>
-						{article.header}
-					</AppLink>
-				</li>
-			))}
-		</ul>
-	</>);
+	const articles = allArticles.slice(
+		(page - 1) * ARTICLES_PER_PAGE,
+		page * ARTICLES_PER_PAGE
+	);
+
+	const nextPageUrl = {
+		search: new URLSearchParams({
+			page: (page + 1).toString(),
+		}).toString()
+	};
+
+	return (
+		<>
+			<h1>Education blog, page {page}</h1>
+			<ul>
+				{articles.map((article, index) => (
+					<li key={index}>
+						<AppLink href={ROUTING.article(article.name)}>
+							{article.header}
+						</AppLink>
+					</li>
+				))}
+			</ul>
+
+			<AppLink href={nextPageUrl}>Next</AppLink>
+		</>
+	);
 }
--- src/app/shared/compontsts/app-link.tsx
+++ src/app/shared/compontsts/app-link.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import {FC, ReactNode} from 'react'
 import styles from './app-link.module.css'
+import { UrlObject } from 'url';
 
 type AppLinkProps = {
     children: ReactNode,
-    href: string
+    href: string | UrlObject
 }
 const AppLink: FC<AppLinkProps> = ({children, href}) => {
     return (
